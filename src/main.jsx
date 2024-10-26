d845da2053a2c62ccb3cd6adf78af73ff614c9bb
zubair040 <zubair040@gmail.com>
2024-10-26 12:48:04 +0500

    Minor Modification

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Add Bootstrap JS
+const script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
+script.async = true;
+document.body.appendChild(script);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
