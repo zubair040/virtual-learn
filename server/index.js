b8c1603d2d8aa993fd65aa1fec3ca3f48cf51650
zubair040 <zubair040@gmail.com>
2024-10-31 10:51:24 +0500

    server error

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-
+const UserModel = require("./models/user")
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/virtual-learn");
+
+app.post('/signup', (req,res) => {
+    UserModel.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.json(err))
+})
 
 app.listen(3001, () => {
     console.log("server is running")
